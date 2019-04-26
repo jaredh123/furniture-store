485519377cd23d6fd85e115f225e6e05bb249b86
jaredh123 <jared.hanson12345@gmail.com>
2019-04-25 20:43:02 -0700

    Reset furniture item ID's, collect types, minor HTML changes

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,26 @@ $(document).ready(function() {
   //   event.preventDefault();
   // });
   
-  
-  
-  
+  var request = new XMLHttpRequest();
+  request.open('GET', "https://it771mq5n2.execute-api.us-west-2.amazonaws.com/production/furniture/", true);
+  request.onload = function() {
+
+    if (request.status >= 200 && request.status < 400) {
+      let body = JSON.parse(this.response);
+      let types = [];
+      
+      for (let i = 0; i < body.body.data.length; i++) {
+        body.body.data[i].id = i;
+        if (!types.includes(body.body.data[i].type)) {
+          types.push(body.body.data[i].type);
+        }
+      }
+      
+      console.log(types);
+      
+    }
+  }
+
+  request.send()
   
 });
